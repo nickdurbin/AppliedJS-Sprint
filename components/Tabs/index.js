4d0242bfe8457927b2d2f624a3053c5d1903668c
nick.durbin <nick.durbin@gmail.com>
2019-08-16 19:28:06 -0400

    Completed the Tabs portion of the sprint. Moving on to the Cards section.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let topics = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then((response) => {
+    topicsArray = response.data.topics;
+    topicsArray.forEach((item) => topics.appendChild(Tabs(item)));
+  })
+  .catch((error) => {
+    console.log('topicsArray', error);
+  })
+
+function Tabs(data) {
+  // Creating the element
+  const tab = document.createElement('div');
+
+  // Adding classes
+  tab.classList.add('tab');
+
+  // Adding textContent
+  tab.textContent = data;
+
+  return tab;
+}
